575516f9b0c4333e92a0ba6314216f3a29ade0f5
Lucas Ferreira Simão <22101918@aluno.cotemig.com.br>
2023-05-18 14:23:56 +0000

    make alphabetic order

--- script.js
+++ script.js
@@ -27,16 +27,17 @@ FetchAPI();
 function loadCountries() {
 	const total = countriesList.reduce((a, b) => a + b.population, 0);
 	countriesTotal.innerText = 'Total: ' + Intl.NumberFormat('pt-BR').format(total);
-	constructList(countriesUl, countriesList);
+	constructList(countriesUl, countriesList, favoriteCountry);
+	console.log(countriesList[0]);
 }
 
 function loadFavorites() {
 	const total = favoritesList.reduce((a, b) => a + b.population, 0);
 	favoritesTotal.innerText = 'Favorito: ' + Intl.NumberFormat('pt-BR').format(total);
-	constructList(favoritesUl, favoritesList);
+	constructList(favoritesUl, favoritesList, UnfavoriteCountry);
 }
 
-function constructList(ul, data) {
+function constructList(ul, data, func) {
 	ul.innerHTML = '';
 
 	const g_li = document.createElement('li');
@@ -64,6 +65,8 @@ function constructList(ul, data) {
 		'<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#000000" viewBox="0 0 256 256"><path d="M178,32c-20.65,0-38.73,8.88-50,23.89C116.73,40.88,98.65,32,78,32A62.07,62.07,0,0,0,16,94c0,70,103.79,126.66,108.21,129a8,8,0,0,0,7.58,0C136.21,220.66,240,164,240,94A62.07,62.07,0,0,0,178,32ZM128,206.8C109.74,196.16,32,147.69,32,94A46.06,46.06,0,0,1,78,48c19.45,0,35.78,10.36,42.6,27a8,8,0,0,0,14.8,0c6.82-16.67,23.15-27,42.6-27a46.06,46.06,0,0,1,46,46C224,147.61,146.24,196.15,128,206.8Z"></path></svg>';
 	g_button.appendChild(g_svg);
 
+	data = data.sort((a, b) => (a.name > b.name ? 1 : b.name > a.name ? -1 : 0));
+
 	data.forEach(({ name, capital, region, flags }, i) => {
 		const li = g_li.cloneNode(false);
 		li.classList.add(`bg-[url('${flags}')]`);
@@ -81,7 +84,7 @@ function constructList(ul, data) {
 
 		const button = g_button.cloneNode(true);
 		button.addEventListener('click', () => {
-			favoriteCountry(i);
+			func(i);
 		});
 
 		div.appendChild(p_name);
@@ -97,6 +100,13 @@ function constructList(ul, data) {
 function favoriteCountry(index) {
 	favoritesList.push(countriesList[index]);
 	countriesList.splice(index, 1);
+	loadCountries();
+	loadFavorites();
+}
+
+function UnfavoriteCountry(index) {
+	countriesList.push(favoritesList[index]);
+	favoritesList.splice(index, 1);
 	loadCountries();
 	loadFavorites();
 }
